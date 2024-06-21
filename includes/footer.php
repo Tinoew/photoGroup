ac98fd91d2ab3f85161df01f5535466718f2f568
Tinoew <177191@student.horizoncollege.nl>
2024-06-21 15:29:30 -0700

    update verandering kwa background

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <div>&copy; Contact: dropperservicex@gmail.com</div>
-    <div>Gemaakt door: Dominique - Tinauw - Calvin</div>
+    <div>Gemaakt door: Dominique - qt - Calvin</div>
 </footer>
