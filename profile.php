36a4fad7ed1698e98f62463d4d5d66f497901289
dominique <157011375+DoomiXX@users.noreply.github.com>
2024-06-24 13:11:51 +0200

    Update (css) css link gefixt

--- profile.php
+++ profile.php
@@ -16,7 +16,7 @@ $conn = dbConnect();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gallery App</title>
-    <link rel="stylesheet" href="../assets/css/profile.css">
+    <link rel="stylesheet" href="./assets/css/profile.css">
 </head>
 <body>
 
--- webshop.php
+++ webshop.php
@@ -11,7 +11,7 @@ $conn = dbConnect();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Webshop</title>
-    <link rel="stylesheet" href="../assets/css/webshop.css">
+    <link rel="stylesheet" href="./assets/css/webshop.css">
 </head>
 <body>
 
