9772536052781bf0a82738183d67f0f375ca7d71
dominique <157011375+DoomiXX@users.noreply.github.com>
2024-06-24 13:35:07 +0200

    update

--- login.php
+++ login.php
@@ -3,6 +3,12 @@ include_once 'includes/navbar.php';
 include_once 'includes/head.php';
 ?>
 
+<?php
+
+include_once 'functions/functions.php';
+$conn = dbConnect();
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
