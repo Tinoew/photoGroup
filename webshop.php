4bf465802d135c8e006f1b40550cbbffffccbee1
calvin01234 <calvinschoolacc@hotmail.com>
2024-07-02 09:09:02 +0200

    Update webshop.php

--- webshop.php
+++ webshop.php
@@ -85,6 +85,81 @@ $conn = dbConnect();
             <h3>city picture 2</h3>
             <p>$30</p>
         </div>
+
+        
+        <div class="grid-item" data-category="nature" data-price="12">
+            <img src="./assets/images/animal.jpg" alt="animal Picture 2">
+            <h3>animal picture</h3>
+            <p>$12</p>
+        </div>
+
+        <div class="grid-item" data-category="animal" data-price="5">
+            <img src="./assets/images/animal.jpg" alt="animal Picture 2">
+            <h3>animalPicture 2</h3>
+            <p>$5</p>
+        </div>
+
+        <div class="grid-item" data-category="city" data-price="9">
+            <img src="./assets/images/city.jpg" alt="city Picture 2">
+            <h3>city picture 3</h3>
+            <p>$9</p>
+        </div>
+
+        
+        <div class="grid-item" data-category="animal" data-price="10">
+            <img src="./assets/images/animal.jpg" alt="Nature Picture 1">
+            <h3>animal Picture 1</h3>
+            <p>$10</p>
+        </div>
+
+        <div class="grid-item" data-category="city" data-price="20">
+            <img src="./assets/images/city.jpg" alt="City Picture 1">
+            <h3>City Picture 1</h3>
+            <p>$20</p>
+        </div>
+
+        <div class="grid-item" data-category="animals" data-price="15">
+            <img src="./assets/images/animals.jpg" alt="Animals Picture 1">
+            <h3>Animals Picture 1</h3>
+            <p>$15</p>
+        </div>
+
+        <div class="grid-item" data-category="nature" data-price="3">
+            <img src="./assets/images/city.jpg" alt="Nature Picture 2">
+            <h3>city picture</h3>
+            <p>$3</p>
+        </div>
+
+        <div class="grid-item" data-category="nature" data-price="24">
+            <img src="./assets/images/nature2.jpg" alt="Nature Picture 2">
+            <h3>Nature Picture 3</h3>
+            <p>$24</p>
+        </div>
+
+        <div class="grid-item" data-category="city" data-price="30">
+            <img src="./assets/images/city.jpg" alt="Nature Picture 2">
+            <h3>city picture 2</h3>
+            <p>$30</p>
+        </div>
+
+        
+        <div class="grid-item" data-category="nature" data-price="45">
+            <img src="./assets/images/nature2.jpg" alt="Nature Picture 2">
+            <h3>Nature Picture 4</h3>
+            <p>$45</p>
+        </div>
+
+        <div class="grid-item" data-category="animal" data-price="35">
+            <img src="./assets/images/animal.jpg" alt="animal Picture 2">
+            <h3>animalPicture 2</h3>
+            <p>$35</p>
+        </div>
+
+        <div class="grid-item" data-category="city" data-price="30">
+            <img src="./assets/images/city.jpg" alt="city Picture 2">
+            <h3>city picture 3</h3>
+            <p>$30</p>
+        </div>
         <!-- Add more grid items as needed -->
     </div>
 </div>
