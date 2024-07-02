ceaf08d9123d14846cd60aad568bcd2baab20335
calvin01234 <calvinschoolacc@hotmail.com>
2024-07-02 08:53:13 +0200

    aaap

--- index.php
+++ index.php
@@ -5,17 +5,21 @@ include_once 'includes/head.php';
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="./style/style.css">
 </head>
-
 <body>
-    <h1>homepage</h1>
+    <!-- Main content -->
+    <div class="main-content">
+        <h1>Welcome to Our Website</h1>
+        <p>This is the homepage of our website. Here you will find the latest updates, news, and features about our products .</p>
+        <p>Feel free to explore and learn more about what we offer.</p>
+        <p>Contact us if you have any questions or need further assistance.</p>
+    </div>
+
     <?php include_once 'includes/footer.php'; ?>
 </body>
-
 </html>
